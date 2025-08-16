7c5169276cd439fd0b2a8c835c6d4b3eb987d19d
PriyankaPandiyan <priyankasuresh857@gmail.com>
2025-08-17 00:33:18 +0530

    Fix Django import path for home view

--- backend/ecommerce/project/project/urls.py
+++ backend/ecommerce/project/project/urls.py
@@ -24,7 +24,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from app.views import home
+
+
 urlpatterns = [
+    path("", home),      
     path('admin/', admin.site.urls),
     path("api/",include("app.urls")),
      # JWT auth token endpoints
