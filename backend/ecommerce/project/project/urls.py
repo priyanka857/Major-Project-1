991fa0b25333c3556307ead9009b8cce74d2df47
PriyankaPandiyan <priyankasuresh857@gmail.com>
2026-09-02 15:21:24 +0530

    Serve media files in production

--- backend/ecommerce/project/project/urls.py
+++ backend/ecommerce/project/project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
@@ -39,4 +39,4 @@ urlpatterns = [
 
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT})]
